5e1697910550f609ccc0c5606085558bbb66981c
anupadkh <alwaysgetwithanup@gmail.com>
2020-08-08 18:31:36 +0545

    fixed_footer False

--- src/app/Layout/Components/footer/footer.component.ts
+++ src/app/Layout/Components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigActions } from '../../../ThemeOptions/store/config.actions';
+import { ThemeOptions } from '../../../theme-options';
 
 @Component({
   selector: 'app-footer',
@@ -6,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public globals: ThemeOptions,
+    public configActions: ConfigActions) {
+  }
 
   ngOnInit() {
+    this.globals.toggleFixedFooter = false
   }
 
 }
